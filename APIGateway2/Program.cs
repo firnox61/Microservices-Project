e291575b40fba7bbe4bf387a742eb5e566039f3a
İSMAİL ENES EROĞLU <73061163+firnox61@users.noreply.github.com>
2024-04-09 15:41:25 +0300

    Microservice mimrarisi çalışıyor

--- APIGateway2/Program.cs
+++ APIGateway2/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Configuration.AddJsonFile("ocelot.json",optional:false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +18,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseOcelot();
+  //  app.UseOcelot();
 }
 
 app.UseHttpsRedirection();
@@ -24,5 +26,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
